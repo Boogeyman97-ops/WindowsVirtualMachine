e8db1ba95fc7030405c72efdd4e1a9d91e3073f2
Boogeyman97-ops <67091478+Boogeyman97-ops@users.noreply.github.com>
2020-08-20 09:45:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 provider "azurerm" {
+  subscription_id = "abb119be-c8dd-42be-9b60-64269a565c61"
+  client_id       = "a20d5aa9-3048-40db-9f4c-a9ca299682cd"
+  client_secret   = "602.UsePMu9clcH4oT~V4ho54Z2L.NUNP."
+  tenant_id       = "426008e7-7e3f-4ad1-ba2d-7fd1e5c4d3b2"
   features {}
 }
 
